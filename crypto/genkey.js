44c661e6d33b59ba072ad07a8d818b5467bc398f
Trevor <sales@merrows.co.uk>
2023-01-06 13:07:07 +0000

    crypto

--- crypto/genkey.js
+++ crypto/genkey.js
@@ -1,4 +1,4 @@
-import {generateKeyPairSync} from 'crypto';
+import {generateKeyPairSync, publicEncrypt, privateDecrypt, createSign, createVerify} from 'crypto';
 
 const {privateKey, publicKey} = generateKeyPairSync('rsa', {
 //const x= generateKeyPairSync('rsa', {
@@ -14,7 +14,25 @@ const {privateKey, publicKey} = generateKeyPairSync('rsa', {
 	},
 
 });
-
+const message = "this is a test";
 //console.log(x);
 console.log(publicKey);
 console.log(privateKey);
+
+const encData = publicEncrypt(publicKey, Buffer.from(message));
+const decData = privateDecrypt(privateKey, Buffer.from(encData));
+
+	console.log(encData.toString('hex'));
+	console.log(decData.toString('utf-8'));
+
+const signer = createSign('rsa-sha256');
+signer.update(message);
+const signature = signer.sign(privateKey, 'hex');
+console.log(signature);
+
+const verifier = createVerify('rsa-sha256');
+verifier.update(message);
+const isVerified = verifier.verify(publicKey, signature, 'hex');
+console.log(isVerified);
+
+
