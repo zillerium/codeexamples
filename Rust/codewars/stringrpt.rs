b7e36363c94d69f21424ab9b9d6ab749e41f8b8f
zillerium <23511764+zillerium@users.noreply.github.com>
2023-03-08 09:08:55 +0000

    Create stringrpt.rs

---
+++ Rust/codewars/stringrpt.rs
@@ -0,0 +1,17 @@
+fn repeat_str(src: &str, count: usize) -> String {
+  
+    let mut owned_string: String = String::new();
+    let borrowed_string: &str = "world";
+    for _i in 0..count {
+        owned_string.push_str(src);
+    }
+    owned_string
+   
+}
+
+#[test]
+fn example_tests() {
+  assert_eq!(repeat_str("a", 4), "aaaa");
+  assert_eq!(repeat_str("hello ", 3), "hello hello hello ");
+  assert_eq!(repeat_str("abc", 2), "abcabc");
+}
