5a8b279c87829be2a53c1293e67ffbab7bec8faa
sales@merrows.co.uk <sales@merrows.co.uk>
2023-03-19 11:01:16 +0000

    add escrow

--- peacio/src/components/PayContract.js
+++ peacio/src/components/PayContract.js
@@ -10,7 +10,7 @@ import {Container, Card, Button, Form, Row, Col} from 'react-bootstrap';
 
 import abi from './abi';
 
-function ApproveEscrowContract() {
+function PayContract() {
 
 	 const  {
                 deployContract, setDeployContract,
@@ -23,6 +23,7 @@ function ApproveEscrowContract() {
                 contractAddress, setContractAddress,
                 contractDetails, setContractDetails,
                 notary, setNotary,
+                sellerAddress, setSellerAddress,
                 contractNumber, setContractNumber,
 		 salesRelease, setSalesRelease,
 		 disputeRelease, setDisputeRelease
@@ -34,16 +35,37 @@ function ApproveEscrowContract() {
 console.log("----items --", items);
 
 	const notaries = [{address:'0x9f0BEA7dE67e8Fb333067ed83b468E5082280835'}];
-        let totAmount = items.reduce((total,item)=>total+item.price*item.quantity,0);
+        let sellers = items.reduce(
+		(acc,item)=> {
+			if (!acc[item.seller]) {
+				acc[item.seller] = {
+                            seller: item.seller,
+				totAmount: item.price*item.quantity,
+				}
+			} else {
+			    acc[item.seller].totAmount +=item.price*item.quantity;
+			}
+	        return acc;
+		}, {});
+	console.log("sellers -- ", sellers);
+	let totAmount=0;
+	let thisSellerAddr = {address:''};
+        Object.entries(sellers).map(([sellerAddress, sellerDetails]) => {
+             thisSellerAddr.address = sellerAddress;		
+             totAmount = sellerDetails.totAmount;
+	})
+
+
 	const stableCoinAmount = totAmount;
         totAmount = totAmount * (10 ** 6);
-        const maxint256 = new BigNumber(2).pow(256).minus(1);
-
+	totAmount = Math.round(totAmount);
+        //const maxint256 = new BigNumber(2).pow(256).minus(1);
+        const maxint256 = 10000000000;
         const genRanNumber = () => {
 
 	  //  const ranNumber = BigNumber.random({crypto:true}).times(maxint256).integerValue();
   //     const ranNumber = new BigNumber(crypto.randomBytes(32).toString('hex'), 16);
-	const ranNumber = new BigNumber(Math.floor(Math.random() * maxint256));
+	const ranNumber = Math.floor(Math.random() * maxint256);
             console.log("---- ran number ---- ", ranNumber);
 		setContractNumber(ranNumber);
         };
@@ -55,15 +77,15 @@ console.log("----items --", items);
 	    setSalesRelease(salesRelease);
 	    setDisputeRelease(disputeRelease);
 	    setNotary(notaries[0]);
-		setContractAddress('0x44f168de1F56c61b93817D5E87e569cEc1f1F8aC');
+		//setContractAddress('0x44f168de1F56c61b93817D5E87e569cEc1f1F8aC');
 	}, []);
-        let argArr = [contractNumber, paySeller, notary, salesRelease, disputeRelease, totAmount  ];
+        let argArr = [contractNumber, thisSellerAddr.address, notary.address, salesRelease, disputeRelease, totAmount  ];
 	console.log("array ---- ", argArr, contractAddress);
         const {config, error} = usePrepareContractWrite({
                    address: contractAddress,
                    abi: abi,
                    functionName: 'approveAndTransferUSDC',
-               //    args:[totAmount]
+                  // args:[contractNumber],
                    args: argArr
         })
         console.log(config);
@@ -78,11 +100,25 @@ console.log("----items --", items);
            setPayContract(false);
         }
 
+    const payEscrow = async () => {
+              console.log("------ pay now--");
+	    try {
+		    console.log("--write", write);
+		    console.log("--config", config);
+		    console.log("--data", data);
+		    console.log("--error", error);
+                const res = await write?.();
+		    console.log("-- res", res);
+	    } catch (err) {
+                console.log("---- err");
+	    }
+    }
+
 
 
     return (
         <>
-        <div><Button  variant="primary" onClick={()=>write?.()}>Pay to Escrow {stableCoinAmount}</Button></div>
+        <div><Button  variant="primary" onClick={payEscrow}>Pay to Escrow {stableCoinAmount}</Button></div>
             {error && (<div> error in formatting {error.message} </div>)}
         </>
     )
@@ -100,5 +136,5 @@ console.log("----items --", items);
 
 
 
-export default ApproveEscrowContract;
+export default PayContract;
 
--- peacio/src/components/WalletInner.js
+++ peacio/src/components/WalletInner.js
@@ -2,7 +2,9 @@ import {useEffect, useState, useContext } from 'react';
 import {Container, Card, Button, Form, Row, Col} from 'react-bootstrap';
 import {CartContext} from '../CartContext';
 import {ContractContext} from './ContractContext';
+import ApproveEscrowContract from './ApproveEscrowContract';
 import PayContract from './PayContract';
+import ApproveContract from './ApproveContract';
 import PaySeller from './PaySeller';
 import ContractShow from './ContractShow';
 
@@ -11,6 +13,7 @@ import ContractShow from './ContractShow';
 function WalletInner(props) {
         const   [deployContract, setDeployContract] = useState(true);
         const   [approveContract, setApproveContract] = useState(false);
+        const   [approveAmount, setApproveAmount] = useState(0);
         const   [payContract, setPayContract] = useState(false);
         const   [approveEscrowContract, setApproveEscrowContract] = useState(false);
         const   [paySeller, setPaySeller] = useState(false);
@@ -19,6 +22,7 @@ function WalletInner(props) {
         const   [contractAddress, setContractAddress] = useState();
         const   [contractDetails, setContractDetails] = useState([{}]);
         const   [notary, setNotary]=useState({address: ''});
+        const   [sellerAddress, setSellerAddress]=useState({address: ''});
         const   [contractNumber, setContractNumber]=useState(0);
         const   [salesRelease, setSalesRelease] = useState(0);
         const   [disputeRelease, setDisputeRelease] = useState(0);
@@ -29,8 +33,14 @@ function WalletInner(props) {
 
 useEffect(() => {
 		setERC20ContractAddress('0x0FA8781a83E46826621b3BC094Ea2A0212e71B23');
-})
+	setContractAddress('0x44f168de1F56c61b93817D5E87e569cEc1f1F8aC');
 
+	setApproveAmount(1000000000000);
+	setApproveContract(true);
+	setApproveEscrowContract(true);
+	setPayContract(true);
+}, [])
+console.log("app amout ------", approveAmount);
 return (
     <div className="container">
 
@@ -45,6 +55,7 @@ return (
                 contractAddress, setContractAddress,
                 contractDetails, setContractDetails,
                 notary, setNotary,
+                sellerAddress, setSellerAddress,
                 salesRelease, setSalesRelease,
                 disputeRelease, setDisputeRelease,
                 contractNumber, setContractNumber
@@ -62,10 +73,19 @@ return (
 	           <h2>Settle Contract</h2>
                </div>
    	   </div>
+
+ <div className="row">
+             <div className="col-6 ">
+        { approveContract && <ApproveContract />        }
+        { !approveContract && <Button variant="secondary" disabled>2. Approve StableCoin Contract</Button>      }
+        </div>
+</div>
+
 	   <div className="row">
                <div className="col-12 text-center">
 
-	             <PayContract />	
+        { payContract && <PayContract />	}
+	{ !payContract && <Button variant="secondary" disabled>4. Pay to Escrow</Button>	}
 
 	           { paySeller && <PaySeller />	}
 	           { !paySeller && <Button variant="secondary" disabled>2. Settle to Seller</Button>	}
