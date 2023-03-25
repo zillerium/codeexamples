b520594d175a1f689c270ae0d0a2e31f87ddc345
sales@merrows.co.uk <sales@merrows.co.uk>
2023-03-25 14:56:07 +0000

    add allowance

--- peacio/src/components/ContractDetails.js
+++ peacio/src/components/ContractDetails.js
@@ -2,60 +2,39 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useContractRead } from 'wagmi';
 import abi from './abi';
 import { ContractContext } from './ContractContext';
+import  ShowContractDetails  from './ShowContractDetails';
 
 function ContractDetails(props) {
-  const { contractAddress } = useContext(ContractContext);
+  const { contractAddress, contractNumber, contractDetails, setContractDetails } = useContext(ContractContext);
 
+	    console.log("----contract number show adata --------------", props.contractNum);
   const config = {
     address: contractAddress,
     abi: abi,
     overrides: { from: props.address },
     functionName: 'salesContracts',
-   args: [props.index],
+   args: [props.contractNum],
   };
 
   const { data, isLoading, isSuccess } = useContractRead(config);
-  const [details, setDetails] = useState({});
 
   useEffect(() => {
     if ((isSuccess)  && (data) ) {
-      const [        buyer,        seller,        notary,        erc20Contract,        paymentAmount,        disputePeriod,        salesPeriod,        salesRelease,        disputeRelease,        status,        sellerApproval,        buyerApproval,      ] = data;
-
-      setDetails({
-        buyer,
-        seller,
-        notary,
-        erc20Contract,
-        paymentAmount,
-        disputePeriod,
-        salesPeriod,
-        salesRelease,
-        disputeRelease,
-        status,
-        sellerApproval,
-        buyerApproval,
-      });
-    }
-  }, [data, isSuccess]);
+	    console.log("----show adata --------------", data);
+	    console.log("----show adata --------------", data);
+	    console.log("----show adata --------------", data);
+	    console.log("----show adata --------------", data);
+      setContractDetails({data});
+      };
+  }, [data]);
 
   if (isLoading) {
     return <div>Loading contract details...</div>;
   }
 
+	 // {contractDetails && <ShowContractDetails /> }
   return (
     <div>
-	  {details && <ShowContractDdetails /> }
-      <p>Seller: {details.seller}</p>
-      <p>Notary: {details.notary}</p>
-      <p>Release Dates:</p>
-      <ul>
-        <li>Sales: {details.salesRelease.toString()}</li>
-        <li>Dispute: {details.disputeRelease.toString()}</li>
-      </ul>
-      <p>Payment Amount: {details.paymentAmount.toString()}</p>
-      <p>Status: {details.status.toString()}</p>
-      <p>Seller Approval: {details.sellerApproval.toString()}</p>
-      <p>Buyer Approval: {details.buyerApproval.toString()}</p>
     </div>
   );
 }
--- peacio/src/components/ContractList.js
+++ peacio/src/components/ContractList.js
@@ -2,24 +2,35 @@ import { useEffect, useState, useContext } from 'react';
 import { useContractRead } from 'wagmi';
 import { ContractContext } from './ContractContext';
 import ContractDetails from './ContractDetails';
+import ShowContractDetails from './ShowContractDetails';
 import abi from './abi';
 
 function ContractList(props) {
-  const { contractAddress, buyerContracts } = useContext(ContractContext);
+  const { contractAddress, contractDetails, buyerContracts } = useContext(ContractContext);
+	const [selContract, setSelContract]=useState();
 console.log("jjjjjjjjjjjjjjj", buyerContracts);
 //           <ContractDetails contractNum={contractNum} />
+          //{contractDetails && <ShowContractDetails /> }
+  const showContractDetailsFunc=(c) => {
+	  setSelContract(c);
+  }
 
   return (
     <div>
       <h3>List of Contracts:</h3>
       <ul>
         {buyerContracts.map((contractNum) => (
-          <li key={contractNum}>
+          <li key={contractNum} onClick={()=>showContractDetailsFunc(contractNum)}>
 		{contractNum}
-           <ContractDetails contractNum={contractNum} />
-          </li>
-        ))}
-      </ul>
+	  </li>
+	))		}
+
+      </ul>				
+  {selContract && (
+           <ContractDetails contractNum={selContract} />
+          //{contractDetails && <ShowContractDetails /> }
+
+        )}
     </div>
   );
 }
