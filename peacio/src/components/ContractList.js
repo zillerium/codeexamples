e63d48738785cff455f93925413d0f6a82562622
sales@merrows.co.uk <sales@merrows.co.uk>
2023-03-27 01:43:37 +0000

    add allowance

--- peacio/src/components/ContractList.js
+++ peacio/src/components/ContractList.js
@@ -11,26 +11,53 @@ function ContractList(props) {
 console.log("jjjjjjjjjjjjjjj", buyerContracts);
 //           <ContractDetails contractNum={contractNum} />
           //{contractDetails && <ShowContractDetails /> }
+console.log("ooooooooooooooooooo contract details ------------", contractDetails);
+console.log("ooooooooooooooooooo contract details ------------", contractDetails);
+console.log("ooooooooooooooooooo contract details ------------", contractDetails);
+console.log("ooooooooooooooooooo contract details ------------", contractDetails);
+console.log("ooooooooooooooooooo contract details ------------", contractDetails);
   const showContractDetailsFunc=(c) => {
+	  console.log("c=====", c);
+	  console.log("c=====", c);
+	  console.log("c=====", c);
 	  setSelContract(c);
+	  console.log("ccontarct=====", selContract);
   }
 
+ /* {selContract && (
+           <ContractDetails contractNum={selContract} />
+  )}
+          {contractDetails && <ShowContractDetails /> }
+*/
+	//  {contractDetails}
   return (
     <div>
       <h3>List of Contracts:</h3>
       <ul>
         {buyerContracts.map((contractNum) => (
-          <li key={contractNum} onClick={()=>showContractDetailsFunc(contractNum)}>
-		{contractNum}
+          <li key={contractNum}>
+		<button onClick={()=>showContractDetailsFunc(contractNum)}>
+		{contractNum}</button>
 	  </li>
 	))		}
 
       </ul>				
-  {selContract && (
-           <ContractDetails contractNum={selContract} />
-          //{contractDetails && <ShowContractDetails /> }
+	  {selContract && <ContractDetails address={props.address} contractNum={selContract}/>}
+	  {contractDetails && JSON.stringify(contractDetails) !== JSON.stringify([{}]) && (
+                <div>
+		  {JSON.stringify(contractDetails)}
+		  seller { contractDetails.data.seller}
+		  buyer { contractDetails.data.buyer}
+		  notary { contractDetails.data.notary}
+		  dispute { contractDetails.data.dispute}
+		  settled { contractDetails.data.settled}
+		  releaseTime { contractDetails.data.releaseTime.toString()}
+		  disputeRelease { contractDetails.data.disputeRelease.toString()}
+		  balance { contractDetails.data.balance.toString()}
+		  price { contractDetails.data.price.toString()}
+		  </div>
 
-        )}
+	  )}
     </div>
   );
 }
