704f7c0210a8218fa53446707204f7cb128f5c13
zillerium <23511764+zillerium@users.noreply.github.com>
2023-03-27 03:01:44 +0100

    Update ContractList.js

--- peacio/src/components/ContractList.js
+++ peacio/src/components/ContractList.js
@@ -4,6 +4,7 @@ import { ContractContext } from './ContractContext';
 import ContractDetails from './ContractDetails';
 import ShowContractDetails from './ShowContractDetails';
 import abi from './abi';
+import { Button, ListGroup, Table } from 'react-bootstrap';
 
 function ContractList(props) {
   const { contractAddress, contractDetails, buyerContracts } = useContext(ContractContext);
@@ -30,34 +31,67 @@ console.log("ooooooooooooooooooo contract details ------------", contractDetails
           {contractDetails && <ShowContractDetails /> }
 */
 	//  {contractDetails}
-  return (
+   
+   return (
     <div>
       <h3>List of Contracts:</h3>
-      <ul>
+      <ListGroup>
         {buyerContracts.map((contractNum) => (
-          <li key={contractNum}>
-		<button onClick={()=>showContractDetailsFunc(contractNum)}>
-		{contractNum}</button>
-	  </li>
-	))		}
+          <ListGroup.Item key={contractNum}>
+            <Button variant="light" onClick={() => showContractDetailsFunc(contractNum)}>
+              {contractNum}
+            </Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
 
-      </ul>				
-	  {selContract && <ContractDetails address={props.address} contractNum={selContract}/>}
-	  {contractDetails && JSON.stringify(contractDetails) !== JSON.stringify([{}]) && (
-                <div>
-		  {JSON.stringify(contractDetails)}
-		  seller { contractDetails.data.seller}
-		  buyer { contractDetails.data.buyer}
-		  notary { contractDetails.data.notary}
-		  dispute { contractDetails.data.dispute}
-		  settled { contractDetails.data.settled}
-		  releaseTime { contractDetails.data.releaseTime.toString()}
-		  disputeRelease { contractDetails.data.disputeRelease.toString()}
-		  balance { contractDetails.data.balance.toString()}
-		  price { contractDetails.data.price.toString()}
-		  </div>
+      {selContract && <ContractDetails address={address} contractNum={selContract} />}
 
-	  )}
+      {contractDetails && JSON.stringify(contractDetails) !== JSON.stringify([{}]) && (
+        <div>
+          <h3>Contract Details:</h3>
+          <Table bordered striped>
+            <tbody>
+              <tr>
+                <td><strong>Seller:</strong></td>
+                <td>{contractDetails.data.seller}</td>
+              </tr>
+              <tr>
+                <td><strong>Buyer:</strong></td>
+                <td>{contractDetails.data.buyer}</td>
+              </tr>
+              <tr>
+                <td><strong>Notary:</strong></td>
+                <td>{contractDetails.data.notary}</td>
+              </tr>
+              <tr>
+                <td><strong>Dispute:</strong></td>
+                <td>{contractDetails.data.dispute}</td>
+              </tr>
+              <tr>
+                <td><strong>Settled:</strong></td>
+                <td>{contractDetails.data.settled}</td>
+              </tr>
+              <tr>
+                <td><strong>Release Time:</strong></td>
+                <td>{new Date(contractDetails.data.releaseTime).toString()}</td>
+              </tr>
+              <tr>
+                <td><strong>Dispute Release:</strong></td>
+                <td>{new Date(contractDetails.data.disputeRelease).toString()}</td>
+              </tr>
+              <tr>
+                <td><strong>Balance:</strong></td>
+                <td>{contractDetails.data.balance.toString()}</td>
+              </tr>
+              <tr>
+                <td><strong>Price:</strong></td>
+                <td>{contractDetails.data.price.toString()}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      )}
     </div>
   );
 }
