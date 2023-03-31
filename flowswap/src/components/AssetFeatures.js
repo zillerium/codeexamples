5cfed4411b749cd4c77a9b8303a90c8ad44fe41b
zillerium <23511764+zillerium@users.noreply.github.com>
2023-03-31 04:10:00 +0100

    Create AssetFeatures.js

---
+++ flowswap/src/components/AssetFeatures.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+const AssetFeatures = ({ features }) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Feature</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Number of Bathrooms</td>
+          <td>{features.numBathrooms}</td>
+        </tr>
+        <tr>
+          <td>Number of Bedrooms</td>
+          <td>{features.numBedrooms}</td>
+        </tr>
+        <tr>
+          <td>House Type</td>
+          <td>{features.houseType}</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+export default AssetFeatures;
