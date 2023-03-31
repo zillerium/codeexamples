58bfc6b89f4d9a03cfe9a2a374f535a0f143a402
zillerium <23511764+zillerium@users.noreply.github.com>
2023-03-31 05:23:02 +0100

    Update AssetFeatures.js

--- flowswap/src/components/AssetFeatures.js
+++ flowswap/src/components/AssetFeatures.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 
-const AssetFeatures = ({ features }) => {
-console.log("features --- ", features);
-	return (
+const AssetFeatures = ({ numBathrooms, numBedrooms, houseType }) => {
+  return (
     <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Feature</th>
-          <th>Value</th>
-        </tr>
-      </thead>
       <tbody>
         <tr>
-          <td>Number of Bathrooms</td>
-          <td>{features.numBathrooms}</td>
-        </tr>
-        <tr>
-          <td>Number of Bedrooms</td>
-          <td>{features.numBedrooms}</td>
-        </tr>
-        <tr>
-          <td>House Type</td>
-          <td>{features.houseType}</td>
+          <td>Number of Bathrooms:</td>
+          <td>{numBathrooms}</td>
+          <td>Number of Bedrooms:</td>
+          <td>{numBedrooms}</td>
+          <td>House Type:</td>
+          <td>{houseType}</td>
         </tr>
       </tbody>
     </Table>
