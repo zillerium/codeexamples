8d2a1f03ccf36b32fa65e438a90c0a64408db735
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-01 08:14:18 +0100

    Update AssetFinancials.js

--- flowswap/src/components/AssetFinancials.js
+++ flowswap/src/components/AssetFinancials.js
@@ -9,22 +9,52 @@ const AssetFinancials = ({
   assetNumberShares,
 }) => {
   return (
-    <Row className="my-4">
-      <Col md={6} className="text-left">
-        <div className="font-weight-bold mb-1">Asset Value</div>
-        <div className="font-weight-bold mb-1">Income</div>
-        <div className="font-weight-bold mb-1">Yield</div>
-        <div className="font-weight-bold mb-1">Risk</div>
-        <div className="font-weight-bold mb-1">Number shares</div>
-      </Col>
-      <Col md={6} className="text-left">
-        <div className="mb-1">{assetValue.toLocaleString()} GBP</div>
-        <div className="mb-1">{assetIncome.toLocaleString()} GBP</div>
-        <div className="mb-1">{assetYield}%</div>
-        <div className="mb-1">{assetRiskRating}</div>
-        <div className="mb-1">{assetNumberShares.toLocaleString()}</div>
-      </Col>
-    </Row>
+    <div className="border border-dark p-3">
+      <Row className="my-4">
+        <Col md={6} className="text-left">
+          <div className="font-weight-bold mb-1">Asset Value</div>
+        </Col>
+        <Col md={6} className="text-left">
+          <div className="mb-1">{assetValue.toLocaleString()} GBP</div>
+        </Col>
+      </Row>
+      <div className="border-bottom"></div>
+      <Row className="my-4">
+        <Col md={6} className="text-left">
+          <div className="font-weight-bold mb-1">Income</div>
+        </Col>
+        <Col md={6} className="text-left">
+          <div className="mb-1">{assetIncome.toLocaleString()} GBP</div>
+        </Col>
+      </Row>
+      <div className="border-bottom"></div>
+      <Row className="my-4">
+        <Col md={6} className="text-left">
+          <div className="font-weight-bold mb-1">Yield</div>
+        </Col>
+        <Col md={6} className="text-left">
+          <div className="mb-1">{assetYield}%</div>
+        </Col>
+      </Row>
+      <div className="border-bottom"></div>
+      <Row className="my-4">
+        <Col md={6} className="text-left">
+          <div className="font-weight-bold mb-1">Risk</div>
+        </Col>
+        <Col md={6} className="text-left">
+          <div className="mb-1">{assetRiskRating}</div>
+        </Col>
+      </Row>
+      <div className="border-bottom"></div>
+      <Row className="my-4">
+        <Col md={6} className="text-left">
+          <div className="font-weight-bold mb-1">Number shares</div>
+        </Col>
+        <Col md={6} className="text-left">
+          <div className="mb-1">{assetNumberShares.toLocaleString()}</div>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
