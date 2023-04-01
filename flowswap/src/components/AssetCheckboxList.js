cc27099d8ba5b80e939150dc39e718ce60463a9f
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-01 08:31:13 +0100

    Update AssetCheckboxList.js

--- flowswap/src/components/AssetCheckboxList.js
+++ flowswap/src/components/AssetCheckboxList.js
@@ -3,17 +3,18 @@ import { Form } from "react-bootstrap";
 
 const AssetCheckboxList = ({ checkboxes }) => {
   return (
-    <div className="d-flex flex-wrap mx-3 w-100">
+    <div className="d-flex flex-row mx-3 w-100">
       {checkboxes.map((checkbox, index) => (
-        <Form.Group key={index} className="mb-0 w-100">
-          <div className="d-flex align-items-center justify-content-end">
+        <Form.Group key={index} className="mb-0">
+          <div className="d-flex align-items-center">
             <Form.Check
               type="checkbox"
               label={checkbox.label}
               checked={checkbox.checked}
               readOnly
-              className="ml-3"
+              className="mr-3"
             />
+            <span>{checkbox.label}</span>
           </div>
         </Form.Group>
       ))}
