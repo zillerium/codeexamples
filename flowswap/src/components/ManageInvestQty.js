e9d674dbd92c9eddaab74df34733ec8de9d847af
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-01 08:50:18 +0100

    Update ManageInvestQty.js

--- flowswap/src/components/ManageInvestQty.js
+++ flowswap/src/components/ManageInvestQty.js
@@ -1,8 +1,17 @@
-// ManageInvestQty.js
 import React from "react";
 import { Form, Col, Button } from "react-bootstrap";
 
 const ManageInvestQty = ({ quantity, onQuantityChange, onAddToCart }) => {
+  const handleIncrease = () => {
+    onQuantityChange(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      onQuantityChange(quantity - 1);
+    }
+  };
+
   return (
     <Form onSubmit={(e) => {
       e.preventDefault();
@@ -10,13 +19,19 @@ const ManageInvestQty = ({ quantity, onQuantityChange, onAddToCart }) => {
     }}>
       <Form.Group controlId="quantity" className="d-flex align-items-center">
         <Form.Label column sm="5">Number Shares to Buy:</Form.Label>
-        <Col sm="5">
+        <Col sm="3">
+          <Button onClick={handleDecrease}>-</Button>
+        </Col>
+        <Col sm="2">
           <Form.Control
             type="number"
             min="1"
             value={quantity}
             onChange={(e) => onQuantityChange(parseInt(e.target.value))}
           />
+        </Col>
+        <Col sm="3">
+          <Button onClick={handleIncrease}>+</Button>
         </Col>
         <Col sm="2">
           <Button type="submit">
