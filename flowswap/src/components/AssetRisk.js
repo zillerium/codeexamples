d81d74dc4bb67efa9b039ee1f6866908570ba054
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 18:32:19 +0100

    Update AssetRisk.js

--- flowswap/src/components/AssetRisk.js
+++ flowswap/src/components/AssetRisk.js
@@ -18,35 +18,35 @@ function AssetRisk() {
   } = useContext(ContractContext);
 
   return (
-    <div style={{ border: "3px solid green", borderRadius: "10px", padding: "10px" }}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
+    <div style={{ border: "3px solid green", borderRadius: "10px", padding: "10px", display: "flex", flexDirection: "column", gap: "20px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
+        <div style={{ flex: "1" }}>
           <input type="number" placeholder="Asset Income" value={assetIncome} 
-            onChange={(e) => setAssetIncome(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setAssetIncome(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
-        <div>
+        <div style={{ flex: "1" }}>
           <input type="number" placeholder="Asset Yield" value={assetYield} 
-            onChange={(e) => setAssetYield(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setAssetYield(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
+        <div style={{ flex: "1" }}>
           <input type="number" placeholder="Asset Risk Rating" value={assetRiskRating} 
-            onChange={(e) => setAssetRiskRating(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setAssetRiskRating(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
-        <div>
+        <div style={{ flex: "1" }}>
           <input type="number" placeholder="Asset Value" value={assetValue} 
-            onChange={(e) => setAssetValue(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setAssetValue(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
+        <div style={{ flex: "1" }}>
           <input type="currency" placeholder="Currency" value={currency} 
-            onChange={(e) => setCurrency(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setCurrency(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
-        <div>
+        <div style={{ flex: "1" }}>
           <input type="number" placeholder="Asset Number of Shares" value={assetNumberShares} 
-            onChange={(e) => setAssetNumberShares(e.target.value)} style={{ border: "3px solid green" }} />
+            onChange={(e) => setAssetNumberShares(e.target.value)} style={{ border: "3px solid green", width: "100%" }} />
         </div>
       </div>
     </div>
