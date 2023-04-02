941cf4013edb929b538fc0ded6b65b0963cfa845
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 19:05:56 +0100

    Update AssetCheckBoxes.js

--- flowswap/src/components/AssetCheckBoxes.js
+++ flowswap/src/components/AssetCheckBoxes.js
@@ -3,36 +3,60 @@ import { ContractContext } from './ContractContext';
 
 function AssetCheckboxes() {
   const {
-    hasDoubleGlazing, setHasDoubleGlazing,
-    hasTenant, setHasTenant,
-    hasGarden, setHasGarden,
-    hasParking, setHasParking,
+    hasDoubleGlazing,
+    setHasDoubleGlazing,
+    hasTenant,
+    setHasTenant,
+    hasGarden,
+    setHasGarden,
+    hasParking,
+    setHasParking,
   } = useContext(ContractContext);
 
   return (
-    <div style={{ border: "2px solid lightgrey", borderRadius: "10px", padding: "10px" }}>
-      <h3 style={{ textAlign: "center" }}>Asset Options</h3>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ flex: "1", marginRight: "5px" }}>
-          <input type="checkbox" id="hasDoubleGlazing" checked={hasDoubleGlazing} 
-            onChange={(e) => setHasDoubleGlazing(e.target.checked)} style={{ marginRight: "5px" }} />
+    <div style={{ border: '2px solid lightgrey', borderRadius: '10px', padding: '10px' }}>
+      <h3 style={{ textAlign: 'center' }}>Asset Options</h3>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ flex: '1', marginRight: '5px' }}>
+          <input
+            type="checkbox"
+            id="hasDoubleGlazing"
+            checked={hasDoubleGlazing}
+            onChange={(e) => setHasDoubleGlazing(e.target.checked)}
+            style={{ marginRight: '5px' }}
+          />
           <label htmlFor="hasDoubleGlazing">Has Double Glazing</label>
         </div>
-        <div style={{ flex: "1", marginLeft: "5px" }}>
-          <input type="checkbox" id="hasTenant" checked={hasTenant} 
-            onChange={(e) => setHasTenant(e.target.checked)} style={{ marginRight: "5px" }} />
+        <div style={{ flex: '1', marginLeft: '5px' }}>
+          <input
+            type="checkbox"
+            id="hasTenant"
+            checked={hasTenant}
+            onChange={(e) => setHasTenant(e.target.checked)}
+            style={{ marginRight: '5px' }}
+          />
           <label htmlFor="hasTenant">Has Tenant</label>
         </div>
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
-        <div style={{ flex: "1", marginRight: "5px" }}>
-          <input type="checkbox" id="hasGarden" checked={hasGarden} 
-            onChange={(e) => setHasGarden(e.target.checked)} style={{ marginRight: "5px" }} />
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+        <div style={{ flex: '1', marginRight: '5px' }}>
+          <input
+            type="checkbox"
+            id="hasGarden"
+            checked={hasGarden}
+            onChange={(e) => setHasGarden(e.target.checked)}
+            style={{ marginRight: '5px' }}
+          />
           <label htmlFor="hasGarden">Has Garden</label>
         </div>
-        <div style={{ flex: "1", marginLeft: "5px" }}>
-          <input type="checkbox" id="hasParking" checked={hasParking} 
-            onChange={(e) => setHasParking(e.target.checked)} style={{ marginRight: "5px" }} />
+        <div style={{ flex: '1', marginLeft: '5px', marginRight: '5px' }}>
+          <input
+            type="checkbox"
+            id="hasParking"
+            checked={hasParking}
+            onChange={(e) => setHasParking(e.target.checked)}
+            style={{ marginRight: '5px' }}
+          />
           <label htmlFor="hasParking">Has Parking</label>
         </div>
       </div>
@@ -41,4 +65,3 @@ function AssetCheckboxes() {
 }
 
 export default AssetCheckboxes;
- 
