000ec6b9345c197d6cfbbd787bb3cd8e26aad814
sales@merrows.co.uk <sales@merrows.co.uk>
2023-04-02 23:45:50 +0000

    add investments

--- flowswap/src/components/AddAssetCall.js
+++ flowswap/src/components/AddAssetCall.js
@@ -11,13 +11,14 @@ function AddAssetCall() {
                  addAsset, assetId, assetValue, assetNumberShares, assetIncome, assetYield, assetRiskRating, currency
                 } = useContext(ContractContext)
 
-
-
+const showAssetButton = !addAsset && assetId> 0 && assetValue > 0 && assetNumberShares > 0 && assetIncome > 0 &&
+                    assetYield > 0 && assetRiskRating > 0 && currency !='';
 
 return ( 
       <>
-	    <div>{!addAsset && assetId> 0 && assetValue > 0 && assetNumberShares > 0 && assetIncome > 0 &&
-		    assetYield > 0 && assetRiskRating > 0 && currency !='' && <AddAsset />}</div>
+	    <div>{showAssetButton ? <AddAsset /> : {<div><Button  variant="primary" disabled=true >Add Asset {assetId} </Button>
+             </div>})}
+ }</div>
 
         </>
     )
