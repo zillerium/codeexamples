17fd6a9ac11a1938bc7ae40a1f791ff363017c5b
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 05:31:25 +0100

    Create cartrow.js

---
+++ flowswap/src/pages/cartrow.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Link } from "react-bootstrap";
+
+const CartRow = ({ item, formatCurrency, onAddOneToCart, onRemoveOneFromCart }) => {
+  const { id, assetAddress, pricePerShare, usdGbpRate, numberSharesToBuy } = item;
+  const subTotalGBP = pricePerShare * numberSharesToBuy;
+  const subTotalUSD = subTotalGBP * usdGbpRate;
+
+  return (
+    <tr>
+      <td>
+        <Link to={`/asset/${id}`}>{assetAddress}</Link>
+      </td>
+      <td>
+        {formatCurrency(pricePerShare)} (
+        {formatCurrency(pricePerShare * usdGbpRate)})
+      </td>
+      <td>{numberSharesToBuy}</td>
+      <td>
+        {formatCurrency(subTotalGBP)} ({formatCurrency(subTotalUSD)})
+      </td>
+      <td>
+        <Button
+          sm="6"
+          onClick={() => onAddOneToCart(id)}
+          className="mx-2"
+        >
+          +
+        </Button>
+        <Button
+          sm="6"
+          onClick={() => onRemoveOneFromCart(id)}
+          className="mx-2"
+        >
+          -
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default CartRow;
