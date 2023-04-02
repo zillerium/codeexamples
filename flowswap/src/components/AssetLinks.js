7cac561aa87706719e91656c904787f8f95b031d
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 18:53:12 +0100

    Update AssetLinks.js

--- flowswap/src/components/AssetLinks.js
+++ flowswap/src/components/AssetLinks.js
@@ -8,14 +8,19 @@ function AssetLinks() {
   } = useContext(ContractContext);
 
   return (
-    <>
-      <div>
-        <input type="text" placeholder="Asset Image URL" value={assetImageUrl} onChange={(e) => setAssetImageUrl(e.target.value)} />
+    <div style={{ border: "2px solid lightgrey", borderRadius: "10px", padding: "10px" }}>
+      <h3 style={{ textAlign: "center" }}>Asset Links</h3>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
+          <input type="text" placeholder="Asset Image URL" value={assetImageUrl} 
+            onChange={(e) => setAssetImageUrl(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
+        </div>
+        <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginLeft: "5px" }}>
+          <input type="text" placeholder="Asset URL" value={assetUrl} 
+            onChange={(e) => setAssetUrl(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
+        </div>
       </div>
-      <div>
-        <input type="text" placeholder="Asset URL" value={assetUrl} onChange={(e) => setAssetUrl(e.target.value)} />
-      </div>
-    </>
+    </div>
   );
 }
 
