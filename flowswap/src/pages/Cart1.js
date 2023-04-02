e3d1e4a4f491b7919830df3e69cf5a01c3b20da9
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 05:19:29 +0100

    Create Cart1.js

---
+++ flowswap/src/pages/Cart1.js
@@ -0,0 +1,106 @@
+import React, { useState, useContext } from "react";
+import { Container, Button, Row, Col, Image, Table } from "react-bootstrap";
+import { CartContext } from "../CartContext";
+import CartTotal from "../components/CartTotal";
+import { useParams, Link } from "react-router-dom";
+
+const formatCurrency = (value) => {
+  return value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+};
+
+const Cart = (props) => {
+  const [search, setSearch] = useState("");
+  const { productId } = useParams();
+  const cart = useContext(CartContext);
+
+  return (
+    <>
+      <div>
+        <h1>Investments </h1>
+      </div>
+      <div>
+        <div>
+          <div>
+            <Table stripod="true" bordered hover>
+              <thead>
+                <tr>
+                  <th>Asset</th>
+                  <th>Price</th>
+                  <th>Number of Shares</th>
+                  <th>SubTot</th>
+                  <th>Cart</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.items.length > 0 &&
+                  cart.items.map((value, key) => {
+                    return (
+                      <tr key={key}>
+                        <td>
+                          <Link
+                            to={{
+                              pathname: `/asset/${value.id}`,
+                              state: { productId: value.id, productPrice: value.price },
+                            }}
+                          >
+                            {value.assetAddress}
+                          </Link>
+                        </td>
+                        <td>{formatCurrency(value.pricePerShare)}</td>
+                        <td>{value.numberSharesToBuy}</td>
+                        <td>
+                          {formatCurrency(value.pricePerShare * value.numberSharesToBuy)}
+                        </td>
+                        <td>
+                          <Button
+                            sm="6"
+                            onClick={() =>
+                              cart.addOneToCart({
+                                dbKey: value.id,
+                                merchantName: value.assetOwnerName,
+                                partSalePrice: value.pricePerShare,
+                                partShortDesc: value.assetAddress,
+                              })
+                            }
+                            className="mx-2"
+                          >
+                            +
+                          </Button>
+                          <Button
+                            sm="6"
+                            onClick={() =>
+                              cart.removeOneFromCart({
+                                dbKey: value.id,
+                                partSalePrice: value.pricePerShare,
+                                partShortDesc: value.assetAddress,
+                              })
+                            }
+                            className="mx-2"
+                          >
+                            -
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+              <tr>
+                <td>Total</td>
+                <td></td>
+                <td></td>
+                <td>{formatCurrency(cart.getTotalCost())}</td>
+                <td>
+                  <Link to="/invest">
+                    <Button disabled={cart.items.length === 0}>Invest</Button>
+                  </Link>
+                </td>
+              </tr>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cart;
