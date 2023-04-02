e84fe460efc5be35e27f76f0c2763f37b3c39ec0
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 12:19:10 +0100

    Update AddAssetMgr.js

--- flowswap/src/components/AddAssetMgr.js
+++ flowswap/src/components/AddAssetMgr.js
@@ -1,6 +1,12 @@
 import {useContext} from 'react'
 import {ContractContext} from './ContractContext'
 import AddAsset from './AddAsset'
+import AssetForm from './AssetForm';
+import AssetCheckboxes from './AssetCheckboxes';
+import AssetDetails from './AssetDetails';
+import AssetLinks from './AssetLinks';
+import AssetRates from './AssetRates';
+import AssetRisk from './AssetRisk';
 
 import {Button} from 'react-bootstrap';
 
@@ -14,11 +20,16 @@ function AddAssetMgr() {
     return (
         <>
 
-        {allowanceAmount}
-        {  sellerAddress && !paySeller && <AddAsset /> }
-        { paySeller && <div>Contract Paid</div> }
-        { !paySeller && <div>Contract unPaid</div> }
-
+    //    {allowanceAmount}
+    //    {  sellerAddress && !paySeller && <AddAsset /> }
+    //    { paySeller && <div>Contract Paid</div> }
+    //    { !paySeller && <div>Contract unPaid</div> }
+      <AssetForm />
+      <AssetCheckboxes />
+       <AssetDetails />
+      <AssetLinks />
+<AssetRates />
+<AssetRisk />
 
         </>
     )
