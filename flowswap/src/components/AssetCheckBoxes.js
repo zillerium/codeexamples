7a41040771d9303e913f12955252d6594ac6d84c
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 19:09:48 +0100

    Update AssetCheckBoxes.js

--- flowswap/src/components/AssetCheckBoxes.js
+++ flowswap/src/components/AssetCheckBoxes.js
@@ -3,61 +3,37 @@ import { ContractContext } from './ContractContext';
 
 function AssetCheckboxes() {
   const {
-    hasDoubleGlazing,
-    setHasDoubleGlazing,
-    hasTenant,
-    setHasTenant,
-    hasGarden,
-    setHasGarden,
-    hasParking,
-    setHasParking,
+    hasDoubleGlazing, setHasDoubleGlazing,
+    hasTenant, setHasTenant,
+    hasGarden, setHasGarden,
+    hasParking, setHasParking,
   } = useContext(ContractContext);
 
   return (
-    <div style={{ border: '2px solid lightgrey', borderRadius: '10px', padding: '10px' }}>
-      <h3 style={{ textAlign: 'center' }}>Asset Options</h3>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ flex: '1', marginRight: '5px' }}>
-          <input
-            type="checkbox"
-            id="hasDoubleGlazing"
-            checked={hasDoubleGlazing}
-            onChange={(e) => setHasDoubleGlazing(e.target.checked)}
-            style={{ marginRight: '5px' }}
-          />
-          <label htmlFor="hasDoubleGlazing">Has Double Glazing</label>
+    <div style={{ border: "2px solid lightgrey", borderRadius: "10px", padding: "10px" }}>
+      <h3 style={{ textAlign: "center" }}>Asset Options</h3>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ flex: "1", marginRight: "5px" }}>
+          <input type="checkbox" id="hasDoubleGlazing" checked={hasDoubleGlazing} 
+            onChange={(e) => setHasDoubleGlazing(e.target.checked)} style={{ marginRight: "5px" }} />
+          <label htmlFor="hasDoubleGlazing" style={{ minWidth: "100px", display: "inline-block" }}>Has Double Glazing</label>
         </div>
-        <div style={{ flex: '1', marginLeft: '5px' }}>
-          <input
-            type="checkbox"
-            id="hasTenant"
-            checked={hasTenant}
-            onChange={(e) => setHasTenant(e.target.checked)}
-            style={{ marginRight: '5px' }}
-          />
-          <label htmlFor="hasTenant">Has Tenant</label>
+        <div style={{ flex: "1", marginLeft: "5px" }}>
+          <input type="checkbox" id="hasTenant" checked={hasTenant} 
+            onChange={(e) => setHasTenant(e.target.checked)} style={{ marginRight: "5px" }} />
+          <label htmlFor="hasTenant" style={{ minWidth: "100px", display: "inline-block" }}>Has Tenant</label>
         </div>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-        <div style={{ flex: '1', marginRight: '5px' }}>
-          <input
-            type="checkbox"
-            id="hasGarden"
-            checked={hasGarden}
-            onChange={(e) => setHasGarden(e.target.checked)}
-            style={{ marginRight: '5px' }}
-          />
-          <label htmlFor="hasGarden">Has Garden</label>
+      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
+        <div style={{ flex: "1", marginRight: "5px" }}>
+          <input type="checkbox" id="hasGarden" checked={hasGarden} 
+            onChange={(e) => setHasGarden(e.target.checked)} style={{ marginRight: "5px" }} />
+          <label htmlFor="hasGarden" style={{ minWidth: "100px", display: "inline-block" }}>Has Garden</label>
         </div>
-        <div style={{ flex: '1', marginLeft: '5px', marginRight: '5px' }}>
-          <input
-            type="checkbox"
-            id="hasParking"
-            checked={hasParking}
-            onChange={(e) => setHasParking(e.target.checked)}
-            style={{ marginRight: '5px' }}
-          />
-          <label htmlFor="hasParking">Has Parking</label>
+        <div style={{ flex: "1", marginLeft: "5px" }}>
+          <input type="checkbox" id="hasParking" checked={hasParking} 
+            onChange={(e) => setHasParking(e.target.checked)} style={{ marginRight: "5px" }} />
+          <label htmlFor="hasParking" style={{ minWidth: "100px", display: "inline-block" }}>Has Parking</label>
         </div>
       </div>
     </div>
