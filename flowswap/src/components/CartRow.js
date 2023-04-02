79d86cf8c84851f9571fad13a2578c856154da08
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-02 07:01:09 +0100

    Update CartRow.js

--- flowswap/src/components/CartRow.js
+++ flowswap/src/components/CartRow.js
@@ -1,65 +1,109 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
 
-const CartRow = ({ cartPrice, currency, item, formatCurrency, onAddOneToCart, onRemoveOneFromCart }) => {
+const CartRow = ({
+  cartPrice,
+  currency,
+  item,
+  formatCurrency,
+  onAddOneToCart,
+  onRemoveOneFromCart
+}) => {
   const { assetId, sellerAddress, assetAddress, pricePerShare, usdGbpRate, numberSharesToBuy } = item;
 
   const [hovering, setHovering] = useState(false);
+  const [showFullRow, setShowFullRow] = useState(false);
 
   const handleMouseEnter = () => {
     setHovering(true);
+    setShowFullRow(true);
   };
 
   const handleMouseLeave = () => {
     setHovering(false);
+    setShowFullRow(false);
   };
 
   const truncatedAddress = sellerAddress.substring(0, 6) + "...";
   return (
-    <tr>
-      <td>
-        <Link to={`/asset/${assetId}`}>{assetId}</Link>
-      </td>
-      <td>
-        <Link to={`/asset/${assetId}`}>{assetAddress}</Link>
-      </td>
-    <td
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      style={{ cursor: "default" }}
-    >
-      {hovering ? sellerAddress : truncatedAddress}
-    </td>
-
-      <td>
-        {formatCurrency(cartPrice)} {currency}
-      </td>
-      <td>{numberSharesToBuy}</td>
-      <td>
-        {formatCurrency(numberSharesToBuy*cartPrice)} {currency}
-      </td>
-      <td>
-        <Button
-          sm="6"
-          onClick={() => onAddOneToCart({assetId: assetId})}
-          className="mx-2"
-        >
-          +
-        </Button>
-        <Button
-          sm="6"
-          onClick={() => onRemoveOneFromCart({assetId: assetId})}
-          className="mx-2"
-        >
-          -
-        </Button>
-      </td>
-    </tr>
+    <>
+      <tr
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{ cursor: "default" }}
+      >
+        <td>
+          <Link to={`/asset/${assetId}`}>{assetId}</Link>
+        </td>
+        <td>
+          <Link to={`/asset/${assetId}`}>{assetAddress}</Link>
+        </td>
+        <td>{hovering ? sellerAddress : truncatedAddress}</td>
+        <td>
+          {formatCurrency(cartPrice)} {currency}
+        </td>
+        <td>{numberSharesToBuy}</td>
+        <td>
+          {formatCurrency(numberSharesToBuy * cartPrice)} {currency}
+        </td>
+        <td>
+          <Button
+            sm="6"
+            onClick={() => onAddOneToCart({ assetId: assetId })}
+            className="mx-2"
+          >
+            +
+          </Button>
+          <Button
+            sm="6"
+            onClick={() => onRemoveOneFromCart({ assetId: assetId })}
+            className="mx-2"
+          >
+            -
+          </Button>
+        </td>
+      </tr>
+      {showFullRow && (
+        <tr>
+          <td colSpan="7">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Asset ID:</td>
+                  <td>{assetId}</td>
+                </tr>
+                <tr>
+                  <td>Asset Address:</td>
+                  <td>{assetAddress}</td>
+                </tr>
+                <tr>
+                  <td>Seller Address:</td>
+                  <td>{sellerAddress}</td>
+                </tr>
+                <tr>
+                  <td>Share Price:</td>
+                  <td>
+                    {formatCurrency(pricePerShare)} {currency}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Number of Shares:</td>
+                  <td>{numberSharesToBuy}</td>
+                </tr>
+                <tr>
+                  <td>Total:</td>
+                  <td>
+                    {formatCurrency(numberSharesToBuy * cartPrice)} {currency}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
 
 export default CartRow;
-
-
-
