2261e92ee5d917751f2d2ccd0fbcc5c840781c16
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-03 01:32:58 +0100

    Update AssetRisk.js

--- flowswap/src/components/AssetRisk.js
+++ flowswap/src/components/AssetRisk.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ContractContext } from './ContractContext';
 
 function AssetRisk() {
@@ -17,44 +17,83 @@ function AssetRisk() {
     setAssetNumberShares,
   } = useContext(ContractContext);
 
-  return (
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipContent, setTooltipContent] = useState("");
+
+  const handleMouseEnter = (event, content) => {
+    setShowTooltip(true);
+    setTooltipContent(content);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+    setTooltipContent("");
+  };
+
+return (
     <div style={{ border: "2px solid lightgrey", borderRadius: "10px", padding: "10px" }}>
       <h3 style={{ textAlign: "center" }}>Asset Financials</h3>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
+          <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
             <input type="number" placeholder="Asset Income" value={assetIncome} 
-              onChange={(e) => setAssetIncome(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
+              onChange={(e) => setAssetIncome(e.target.value)} 
+              onMouseEnter={(e) => handleMouseEnter(e, assetIncome)} 
+              onMouseLeave={handleMouseLeave}
+              style={{ border: "none", width: "100%", padding: "5px" }} />
+            {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
           </div>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
+          <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
             <input type="number" placeholder="Asset Yield" value={assetYield} 
-              onChange={(e) => setAssetYield(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
+              onChange={(e) => setAssetYield(e.target.value)} 
+              onMouseEnter={(e) => handleMouseEnter(e, assetYield)} 
+              onMouseLeave={handleMouseLeave}
+              style={{ border: "none", width: "100%", padding: "5px" }} />
+            {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
           </div>
         </div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
-            <input type="number" placeholder="Asset Risk Rating" value={assetRiskRating} 
-              onChange={(e) => setAssetRiskRating(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
-          </div>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
-            <input type="number" placeholder="Asset Value" value={assetValue} 
-              onChange={(e) => setAssetValue(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
-          </div>
-        </div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
-            <input type="currency" placeholder="Currency" value={currency} 
-              onChange={(e) => setCurrency(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
-          </div>
-          <div style={{ borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
-            <input type="number" placeholder="Asset Number of Shares" value={assetNumberShares} 
-              onChange={(e) => setAssetNumberShares(e.target.value)} style={{ border: "none", width: "100%", padding: "5px" }} />
-          </div>
-        </div>
+         
+
+
+<div style={{ display: "flex", justifyContent: "space-between" }}>
+  <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
+    <input type="number" placeholder="Asset Risk Rating" value={assetRiskRating} 
+      onChange={(e) => setAssetRiskRating(e.target.value)} 
+      onMouseEnter={(e) => handleMouseEnter(e, assetRiskRating)} 
+      onMouseLeave={handleMouseLeave}
+      style={{ border: "none", width: "100%", padding: "5px" }} />
+    {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
+  </div>
+  <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
+    <input type="number" placeholder="Asset Value" value={assetValue} 
+      onChange={(e) => setAssetValue(e.target.value)} 
+      onMouseEnter={(e) => handleMouseEnter(e, assetValue)} 
+      onMouseLeave={handleMouseLeave}
+      style={{ border: "none", width: "100%", padding: "5px" }} />
+    {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
+  </div>
+</div>
+<div style={{ display: "flex", justifyContent: "space-between" }}>
+  <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1", marginRight: "5px" }}>
+    <input type="currency" placeholder="Currency" value={currency} 
+      onChange={(e) => setCurrency(e.target.value)} 
+      onMouseEnter={(e) => handleMouseEnter(e, currency)} 
+      onMouseLeave={handleMouseLeave}
+      style={{ border: "none", width: "100%", padding: "5px" }} />
+    {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
+  </div>
+  <div style={{ position: "relative", borderRadius: "10px", border: "1px solid lightgrey", flex: "1" }}>
+    <input type="number" placeholder="Asset Number of Shares" value={assetNumberShares} 
+      onChange={(e) => setAssetNumberShares(e.target.value)} 
+      onMouseEnter={(e) => handleMouseEnter(e, assetNumberShares)} 
+      onMouseLeave={handleMouseLeave}
+      style={{ border: "none", width: "100%", padding: "5px" }} />
+    {showTooltip && <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", background: "white", padding: "5px", borderRadius: "5px", boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.3)" }}>{tooltipContent}</div>}
+  </div>
+</div>
       </div>
     </div>
   );
 }
 
 export default AssetRisk;
-
