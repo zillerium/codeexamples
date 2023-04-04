6b3d6e0dfaaf5e946de4669396cb6c430e7f7724
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 08:20:05 +0100

    Update AddPdfNew.js

--- flowswap/src/components/AddPdfNew.js
+++ flowswap/src/components/AddPdfNew.js
@@ -1,38 +1,32 @@
-import React, { useContext } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "react-bootstrap";
 import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import { ContractContext } from "./ContractContext";
-
-function getImgFromUrl(imgUrl, callback) {
-  var img = new Image();
-  img.src = imgUrl;
-  img.onload = function () {
-    callback(img);
-  };
-}
-
-function generatePDF(img) {
-  var options = { orientation: "p", unit: "mm", format: "a4" };
-  var doc = new jsPDF(options);
-
-  doc.addImage(img, "JPEG", 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
-
-  doc.save("asset_data.pdf");
-}
 
 function AddPdf() {
   const { assetImageUrl } = useContext(ContractContext);
+  const [imageDataUrl, setImageDataUrl] = useState("");
+  const imageRef = useRef(null);
 
-  const generatePDFWithImage = () => {
-    // Create a new jsPDF instance
-    getImgFromUrl(assetImageUrl, (img) => {
-      generatePDF(img);
+  const handleGeneratePDF = () => {
+    html2canvas(imageRef.current).then((canvas) => {
+      const imgData = canvas.toDataURL("image/jpeg");
+      setImageDataUrl(imgData);
+      generatePDF(imgData);
     });
+  };
+
+  const generatePDF = (imgData) => {
+    var doc = new jsPDF();
+    doc.addImage(imgData, "JPEG", 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
+    doc.save("asset_data.pdf");
   };
 
   return (
     <>
-      <Button variant="primary" onClick={generatePDFWithImage}>
+      <img ref={imageRef} src={assetImageUrl} alt="asset" />
+      <Button variant="primary" onClick={handleGeneratePDF}>
         Generate PDF
       </Button>
     </>
