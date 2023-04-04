3110df43400d4ce176e545db084a51a21396c646
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 06:29:52 +0100

    Update AddPdf.js

--- flowswap/src/components/AddPdf.js
+++ flowswap/src/components/AddPdf.js
@@ -93,3 +93,101 @@ console.log("assetImageurl - ");
 }
 
 export default AddPdf;
+
+import React, { useState, useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { ContractContext } from './ContractContext';
+
+// Load the image
+function getImgFromUrl(logo_url, callback) {
+  var img = new Image();
+  img.src = logo_url;
+  img.onload = function () {
+    callback(img);
+  };
+}
+
+// In the onload event on first step, make a callback to use the jspdf doc
+function generatePDF(img) {
+  var options = { orientation: 'p', unit: 'mm', format: 'a4' };
+  var doc = new jsPDF(options);
+  doc.addImage(img, 'JPEG', 0, 0, 100, 50);
+	
+	 doc.setFontSize(12);
+    doc.setFont('normal');
+console.log("assetid === ", assetId);
+    // Add the asset data to the document
+ doc.text(`Asset ID: ${assetId}`, 20, 20);
+    doc.text(`Owner Name: ${assetOwnerName}`, 20, 30);
+    doc.text(`Address: ${assetAddress}`, 20, 40);
+    doc.text(`Preferred Notary: ${assetPreferredNotary}`, 20, 50);
+    doc.text(`Seller Address: ${sellerAddress}`, 20, 60);
+    doc.text(`Has Double Glazing: ${hasDoubleGlazing}`, 20, 70);
+    doc.text(`Has Tenant: ${hasTenant}`, 20, 80);
+    doc.text(`Has Garden: ${hasGarden}`, 20, 90);
+    doc.text(`Has Parking: ${hasParking}`, 20, 100);
+    doc.text(`Number of Bathrooms: ${assetNumberBathrooms}`, 20, 110);
+    doc.text(`Number of Bedrooms: ${assetNumberBedrooms}`, 20, 120);
+    doc.text(`House Type: ${assetHouseType}`, 20, 130);
+    doc.text(`Image URL: ${assetImageUrl}`, 20, 140);
+    doc.text(`Asset URL: ${assetUrl}`, 20, 150);
+    doc.text(`Value (${currency}): ${assetValue}`, 20, 160);
+    doc.text(`Number of Shares: ${assetNumberShares}`, 20, 170);
+    doc.text(`Income (${currency}): ${assetIncome}`, 20, 180);
+    doc.text(`Yield: ${assetYield}`, 20, 190);
+    doc.text(`Risk Rating: ${assetRiskRating}`, 20, 200);
+    doc.text(`USD/GBP Rate: ${usdGbpRate}`, 20, 210);
+    doc.text(`Number of Shares Sold: ${assetNumberSharesSold}`, 20, 220); 
+    // Add other asset details as needed...
+	
+	
+  doc.save('asset_data.pdf');
+}
+
+function AddPdf() {
+  const {
+    assetId,
+    dbKey,
+    assetOwnerName,
+    assetAddress,
+    assetValue,
+    assetNumberShares,
+    hasTenant,
+    hasGarden,
+    hasParking,
+    assetImageUrl,
+    assetUrl,
+    assetIncome,
+    assetYield,
+    assetNumberBathrooms,
+    assetNumberBedrooms,
+    assetHouseType,
+    hasDoubleGlazing,
+    assetRiskRating,
+    assetPreferredNotary,
+    currency,
+    usdGbpRate,
+    assetNumberSharesSold,
+    sellerAddress,
+  } = useContext(ContractContext);
+
+  const generatePDFWithImage = (img) => {
+    // Create a new jsPDF instance
+    getImgFromUrl(assetImageUrl, (img) => {
+      generatePDF(img);
+    });
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={generatePDFWithImage}>
+        Generate PDF
+      </Button>
+    </>
+  );
+}
+
+export default AddPdf;
+
