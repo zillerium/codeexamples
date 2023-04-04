b448b8431a7a14a81c86bd09e1605f6164e98e49
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 08:49:56 +0100

    Update AddPdfNew.js

--- flowswap/src/components/AddPdfNew.js
+++ flowswap/src/components/AddPdfNew.js
@@ -1,27 +1,17 @@
-import React, { useState, useRef } from 'react';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import React from 'react';
+import MyDocument from './MyDocument';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 
-function App() {
-  const [assetImageUrl, setAssetImageUrl] = useState('https://i.pinimg.com/originals/c7/69/91/c7699158f7213d6b38a7c55b81c1af07.jpg');
-  const imageRef = useRef(null);
-
-  const handleSaveAsPDF = () => {
-    const input = imageRef.current;
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, 'PNG', 0, 0);
-      pdf.save('download.pdf');
-    });
-  };
+const App = () => {
+  const imageUrl = 'https://i.imgur.com/nf8nLCh.jpeg';
 
   return (
     <div>
-      <img ref={imageRef} src={assetImageUrl} alt="asset" />
-      <button onClick={handleSaveAsPDF}>Save to PDF</button>
+      <PDFDownloadLink document={<MyDocument imageUrl={imageUrl} />} fileName="my-document.pdf">
+        {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
+      </PDFDownloadLink>
     </div>
   );
-}
+};
 
 export default App;
