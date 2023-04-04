d89f4bfd7eef9f56b637596b6cc580e81f009a37
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 09:58:47 +0100

    Update AddPdf.js

--- flowswap/src/components/AddPdf.js
+++ flowswap/src/components/AddPdf.js
@@ -8,26 +8,47 @@ function AddPdf() {
   const { assetImageUrl } = useContext(ContractContext);
   const screenshotRef = useRef(null);
   const [photo, setPhoto] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false); // add state to track image load status
 
   const onChangephoto = (e) => {
     setPhoto(e.target.files[0]);
   };
 
-  const generatePDF1 = () => {
+  const generatePDF = () => {
     html2canvas(screenshotRef.current).then((canvas) => {
-      const imgData = canvas.toDataURL('image/jpg');
+      const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF();
       const imgProps = pdf.getImageProperties(imgData);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.addImage(
+        imgData,
+        'PNG',
+        0,
+        0,
+        pdfWidth,
+        pdfHeight,
+        undefined,
+        undefined,
+        { preserveAspectRatio: true }
+      );
       if (photo) {
         const reader = new FileReader();
         reader.readAsDataURL(photo);
         reader.onload = () => {
           const imgData = reader.result;
           pdf.addPage();
-          pdf.addImage(imgData, 'JPEG', 0, 0, pdfWidth, pdfHeight);
+          pdf.addImage(
+            imgData,
+            'JPEG',
+            0,
+            0,
+            pdfWidth,
+            pdfHeight,
+            undefined,
+            undefined,
+            { preserveAspectRatio: true }
+          );
           pdf.save('my-image.pdf');
         };
       } else {
@@ -35,92 +56,28 @@ function AddPdf() {
       }
     });
   };
-  
-  const generatePDF2 = () => {
-  html2canvas(screenshotRef.current).then((canvas) => {
-    const imgData = canvas.toDataURL('image/jpg');
-    const pdf = new jsPDF();
-    const imgProps = pdf.getImageProperties(imgData);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    pdf.setMargins(0, 0, 0, 0); // set margins to zero
-    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    if (photo) {
-      const reader = new FileReader();
-      reader.readAsDataURL(photo);
-      reader.onload = () => {
-        const imgData = reader.result;
-        pdf.addPage();
-        pdf.addImage(imgData, 'JPEG', 0, 0, pdfWidth, pdfHeight);
-        pdf.save('my-image.pdf');
-      };
-    } else {
-      pdf.save('my-image.pdf');
-    }
-  });
-};
-  
-  const generatePDF = () => {
-  html2canvas(screenshotRef.current).then((canvas) => {
-    const imgData = canvas.toDataURL('image/png');
-    const pdf = new jsPDF();
-    const imgProps = pdf.getImageProperties(imgData);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    pdf.addImage(
-      imgData,
-      'PNG',
-      0,
-      0,
-      pdfWidth,
-      pdfHeight,
-      undefined,
-      undefined,
-      { preserveAspectRatio: true } // add preserveAspectRatio option
-    );
-    if (photo) {
-      const reader = new FileReader();
-      reader.readAsDataURL(photo);
-      reader.onload = () => {
-        const imgData = reader.result;
-        pdf.addPage();
-        pdf.addImage(
-          imgData,
-          'JPEG',
-          0,
-          0,
-          pdfWidth,
-          pdfHeight,
-          undefined,
-          undefined,
-          { preserveAspectRatio: true } // add preserveAspectRatio option
-        );
-        pdf.save('my-image.pdf');
-      };
-    } else {
-      pdf.save('my-image.pdf');
-    }
-  });
-};
-
-
 
   return (
     <div>
-      <div ref={screenshotRef}>
-        <img src={assetImageUrl} alt="My Image" />
-      </div>
-      <div>
-        <input
-          type="file"
-          name="photo"
-          onChange={onChangephoto}
-          accept="image/png, image/png, image/jpeg, image/jpg"
-        />
-        <Button variant="primary" onClick={generatePDF}>
-          Save as PDF
-        </Button>
-      </div>
+      {isLoaded && ( // render image and html2canvas only after image is loaded
+        <div ref={screenshotRef}>
+          <img onLoad={() => setIsLoaded(true)} src={assetImageUrl} alt="My Image" />
+        </div>
+      )}
+      {!isLoaded && <p>Loading image...</p>}
+      {isLoaded && (
+        <div>
+          <input
+            type="file"
+            name="photo"
+            onChange={onChangephoto}
+            accept="image/png, image/png, image/jpeg, image/jpg"
+          />
+          <Button variant="primary" onClick={generatePDF}>
+            Save as PDF
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
