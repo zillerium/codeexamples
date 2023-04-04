9817d3e6cd4f955c6a7044dc611840fc3efa9d0f
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 01:15:38 +0100

    Update AddPdf.js

--- flowswap/src/components/AddPdf.js
+++ flowswap/src/components/AddPdf.js
@@ -1,54 +1,94 @@
-import {ContractContext} from './ContractContext'
-
-import React, {useState, useContext} from 'react';
-
-import {Button} from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { ContractContext } from './ContractContext';
 
 function AddPdf() {
+  const {
+    assetId,
+    dbKey,
+    assetOwnerName,
+    assetAddress,
+    assetValue,
+    assetNumberShares,
+    hasTenant,
+    hasGarden,
+    hasParking,
+    assetImageUrl,
+    assetUrl,
+    assetIncome,
+    assetYield,
+    assetNumberBathrooms,
+    assetNumberBedrooms,
+    assetHouseType,
+    hasDoubleGlazing,
+    assetRiskRating,
+    assetPreferredNotary,
+    currency,
+    usdGbpRate,
+    assetNumberSharesSold,
+    sellerAddress,
+  } = useContext(ContractContext);
 
-     const  {
-          assetId: assetId,
-          dbKey: dbKey,
-          assetOwnerName: assetOwnerName,
-          assetAddress: assetAddress,
-          assetValue: assetValue,
-          assetNumberShares: assetNumberShares,
-          hasTenant: hasTenant,
-          hasGarden: hasGarden,
-          hasParking: hasParking,
-          assetImageUrl: assetImageUrl,
-          assetUrl: assetUrl,
-          assetIncome: assetIncome,
-          assetYield: assetYield,
-          assetNumberBathrooms: assetNumberBathrooms,
-          assetNumberBedrooms: assetNumberBedrooms,
-          assetHouseType: assetHouseType,
-          hasDoubleGlazing: hasDoubleGlazing,
-          assetRiskRating: assetRiskRating,
-          assetPreferredNotary: assetPreferredNotary,
-          currency: currency,
-                              usdGbpRate: usdGbpRate,
-              assetNumberSharesSold: assetNumberSharesSold,
-              sellerAddress: sellerAddress,
-                } = useContext(ContractContext)
-/*
-const showAssetButton = !addAsset && assetId> 0 && assetValue > 0 && assetNumberShares > 0 && assetIncome > 0 &&
-                    assetYield > 0 && assetRiskRating > 0 && currency !='';
+  const generatePDF = () => {
+    // Create a new jsPDF instance
+    const doc = new jsPDF();
 
-	    {showAssetButton ? <AddAsset /> : <div><Button  variant="primary" disabled >Add Asset {assetId} </Button>
-            </div> }
-*/
-return ( 
-      <>
-	{
+    // Set the font size and style for the document
+    doc.setFontSize(12);
+    doc.setFontStyle('normal');
 
-	}
- 
+    // Add the asset data to the document
+    doc.text(`Asset ID: ${assetId}`, 20, 20);
+    doc.text(`Owner Name: ${assetOwnerName}`, 20, 30);
+    doc.text(`Address: ${assetAddress}`, 20, 40);
+	  doc.text(`assetPreferredNotary: ${assetPreferredNotary}`, 20, 40);
+	    doc.text(`sellerAddress: ${sellerAddress}`, 20, 150);
+	    doc.text(`hasDoubleGlazing: ${hasDoubleGlazing}`, 20, 150);
+	      doc.text(`Has Tenant: ${hasTenant}`, 20, 70);
+    doc.text(`Has Garden: ${hasGarden}`, 20, 80);
+    doc.text(`Has Parking: ${hasParking}`, 20, 90);
+     doc.text(`assetNumberBathrooms: ${assetNumberBathrooms}`, 20, 130);
+	      doc.text(`assetNumberBedrooms: ${assetNumberBedrooms}`, 20, 140);
+	      doc.text(`assetHouseType: ${assetHouseType}`, 20, 150);	  
+	  doc.text(`imageUrl: ${imageUrl}`, 20, 100);
+	    doc.text(`assetUrl: ${assetUrl}`, 20, 100);
+	  	     doc.text(`usdGbpRate: ${usdGbpRate}`, 20, 150);
+	       doc.text(`assetNumberSharesSold: ${assetNumberSharesSold}`, 20, 150);
+	  	    
+	      doc.text(`assetIncome: ${assetIncome}`, 20, 110);
+	      doc.text(`assetYield: ${assetYield}`, 20, 120);
+	    doc.text(`assetRiskRating: ${assetRiskRating}`, 20, 150);
+	 
+	  
+    doc.text(`Value: ${assetValue}`, 20, 50);
+	   doc.text(`currency: ${currency}`, 20, 150);
+    doc.text(`Number of Shares: ${assetNumberShares}`, 20, 60);
 
-        </>
-    )
+  
+    
+    // Add other asset details as needed...
 
+    // Add the image to the document
+    html2canvas(document.querySelector(`img[src='${assetImageUrl}']`)).then(
+      canvas => {
+        const imgData = canvas.toDataURL('image/jpeg');
+        doc.addImage(imgData, 'JPEG', 20, 100, 100, 75);
+
+        // Save the document as a PDF file
+        doc.save('asset_data.pdf');
+      }
+    );
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={generatePDF}>
+        Generate PDF
+      </Button>
+    </>
+  );
 }
 
 export default AddPdf;
-
