d260fce451097febe3ea0b099c7da1b0b8fb31d9
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 14:57:41 +0100

    Update LoadIpfs.js

--- flowswap/src/components/LoadIpfs.js
+++ flowswap/src/components/LoadIpfs.js
@@ -1,6 +1,7 @@
 import React, { useRef, useContext, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import jsPDF from 'jspdf';
+import { create } from 'ipfs-http-client';
 import { ContractContext } from './ContractContext';
 
 function LoadIpfs() {
@@ -8,34 +9,40 @@ function LoadIpfs() {
     ipfsHash, setIpfsHash,
   } = useContext(ContractContext);
 
-	function loadIpfsPdf() {
-
-
-	}
-
   const [pdf, setPdf] = useState(null);
 
   const onChangePdf = (e) => {
     setPdf(e.target.files[0]);
   };
 
+  const loadIpfsPdf = async () => {
+    const ipfs = create({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+    });
 
+    const pdfFile = await pdf.arrayBuffer();
+    const { cid } = await ipfs.add(pdfFile);
+
+    setIpfsHash(cid.toString());
+  };
 
   return (
     <div>
-	  <div>Load PDF to Ipfs</div>
+      <div>Load PDF to Ipfs</div>
       <div>
-	   <input
+        <input
           type="file"
           name="pdfFile"
           onChange={onChangePdf}
-          accept="pdf"
+          accept=".pdf"
         />
         <Button variant="primary" onClick={loadIpfsPdf}>
           Load Pdf to Ipfs
         </Button>
       </div>
-
+      {ipfsHash && <div>IPFS hash: {ipfsHash}</div>}
     </div>
   );
 }
