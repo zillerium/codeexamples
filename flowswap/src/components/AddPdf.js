7034ced30a6d2f8d6fff3994bf028178e5079b50
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 11:49:23 +0100

    Update AddPdf.js

--- flowswap/src/components/AddPdf.js
+++ flowswap/src/components/AddPdf.js
@@ -1,52 +1,10 @@
-import React, { useRef, useContext, useState } from 'react';
-import { Button } from 'react-bootstrap';
-import jsPDF from 'jspdf';
-import { ContractContext } from './ContractContext';
-
-function AddPdf() {
-  const {
-    assetId,
-    dbKey,
-    assetOwnerName,
-    assetAddress,
-    assetValue,
-    assetNumberShares,
-    hasTenant,
-    hasGarden,
-    hasParking,
-    assetImageUrl,
-    assetUrl,
-    assetIncome,
-    assetYield,
-    assetNumberBathrooms,
-    assetNumberBedrooms,
-    assetHouseType,
-    hasDoubleGlazing,
-    assetRiskRating,
-    assetPreferredNotary,
-    currency,
-    usdGbpRate,
-    assetNumberSharesSold,
-    sellerAddress,
-  } = useContext(ContractContext);
-
-
- 
-   const screenshotRef = useRef(null);
-  const [photo, setPhoto] = useState(null);
-
-  const onChangephoto = (e) => {
-    setPhoto(e.target.files[0]);
-  };
-
-  
-   function generatePDF() {
+function generatePDF() {
   const pdf = new jsPDF('p', 'pt', 'a4');
   const width = pdf.internal.pageSize.getWidth();
   const height = pdf.internal.pageSize.getHeight();
 
   pdf.setFontSize(24);
-  pdf.text('Flowswap', 20, 40);
+  pdf.text('Flowswap', width / 2, 40, { align: 'center' });
 
   // Add image
   if (photo) {
@@ -72,75 +30,44 @@ function AddPdf() {
         imgHeight * scaleFactor
       );
 
+      const fieldsPerPage = Math.floor((height - y) / m) - 1;
+      let fieldCount = 0;
+
+      const printText = (text) => {
+        if (fieldCount >= fieldsPerPage) {
+          pdf.addPage();
+          y = 60;
+          fieldCount = 0;
+        }
+        pdf.text(text, x, y);
+        y += m;
+        fieldCount++;
+      };
+
       // Add text fields
-      pdf.text(`Asset ID: ${assetId}`, x, y);
-      pdf.text(`Owner Name: ${assetOwnerName}`, x, y += m);
-      pdf.text(`Address: ${assetAddress}`, x, y += m);
-      pdf.text(`Preferred Notary: ${assetPreferredNotary}`, x, y += m);
-      pdf.text(`Seller Address: ${sellerAddress}`, x, y += m);
-      pdf.text(`Has Double Glazing: ${hasDoubleGlazing}`, x, y += m);
-      pdf.text(`Has Tenant: ${hasTenant}`, x, y += m);
-      pdf.text(`Has Garden: ${hasGarden}`, x, y += m);
-      pdf.text(`Has Parking: ${hasParking}`, x, y += m);
-      pdf.text(`Number of Bathrooms: ${assetNumberBathrooms}`, x, y += m);
-      pdf.text(`Number of Bedrooms: ${assetNumberBedrooms}`, x, y += m);
-      pdf.text(`House Type: ${assetHouseType}`, x, y += m);
-      pdf.text(`Image URL: ${assetImageUrl}`, x, y += m);
-      pdf.text(`Asset URL: ${assetUrl}`, x, y += m);
-      pdf.text(`Value (${currency}): ${assetValue}`, x, y += m);
-      pdf.text(`Number of Shares: ${assetNumberShares}`, x, y += m);
-      pdf.text(`Income (${currency}): ${assetIncome}`, x, y += m);
-      pdf.text(`Yield: ${assetYield}`, x, y += m);
-      pdf.text(`Risk Rating: ${assetRiskRating}`, x, y += m);
-      pdf.text(`USD/GBP Rate: ${usdGbpRate}`, x, y += m);
-      pdf.text(`Number of Shares Sold: ${assetNumberSharesSold}`, x, y += m);
+      printText(`Asset ID: ${assetId}`);
+      printText(`Owner Name: ${assetOwnerName}`);
+      printText(`Address: ${assetAddress}`);
+      printText(`Preferred Notary: ${assetPreferredNotary}`);
+      printText(`Seller Address: ${sellerAddress}`);
+      printText(`Has Double Glazing: ${hasDoubleGlazing}`);
+      printText(`Has Tenant: ${hasTenant}`);
+      printText(`Has Garden: ${hasGarden}`);
+      printText(`Has Parking: ${hasParking}`);
+      printText(`Number of Bathrooms: ${assetNumberBathrooms}`);
+      printText(`Number of Bedrooms: ${assetNumberBedrooms}`);
+      printText(`House Type: ${assetHouseType}`);
+      printText(`Image URL: ${assetImageUrl}`);
+      printText(`Asset URL: ${assetUrl}`);
+      printText(`Value (${currency}): ${assetValue}`);
+      printText(`Number of Shares: ${assetNumberShares}`);
+      printText(`Income (${currency}): ${assetIncome}`);
+      printText(`Yield: ${assetYield}`);
+      printText(`Risk Rating: ${assetRiskRating}`);
+      printText(`USD/GBP Rate: ${usdGbpRate}`);
+      printText(`Number of Shares Sold: ${assetNumberSharesSold}`);
 
       pdf.save('imagepdf.pdf');
     };
   }
-
 }
-
-  const generatePDF2 = () => {
-    const pdf = new jsPDF('p', 'pt', 'a4');
-    const w = pdf.internal.pageSize.getWidth();
-    const h = pdf.internal.pageSize.getHeight();
-    const img = new Image();
-    img.src = URL.createObjectURL(photo);
-    img.onload = function () {
-      const imgWidth = this.width;
-      const imgHeight = this.height;
-      const scaleFactor = Math.min(w / imgWidth, h / imgHeight);
-      pdf.addImage(
-        img,
-        'JPEG',
-        0,
-        0,
-        imgWidth * scaleFactor,
-        imgHeight * scaleFactor
-      );
-      pdf.save('imagepdf.pdf');
-    };
-  };
-
-  return (
-    <div>
-      <div ref={screenshotRef}>
-        <img src={assetImageUrl} alt="My Image" />
-      </div>
-      <div>
-        <input
-          type="file"
-          name="photo"
-          onChange={onChangephoto}
-          accept="image/png, image/png, image/jpeg, image/jpg"
-        />
-        <Button variant="primary" onClick={generatePDF}>
-          Save as PDF
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-export default AddPdf;
