b767cb910a1455c9d6aa3f8029c8e6be94729999
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-04 09:03:01 +0100

    Update AddPdf.js

--- flowswap/src/components/AddPdf.js
+++ flowswap/src/components/AddPdf.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import MyDocument from './MyDocument';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import React, { useRef, useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { Document, Page, Image, pdf } from '@react-pdf/renderer';
+import { ContractContext } from './ContractContext';
 
-const App = () => {
-  const imageUrl = 'https://i.imgur.com/nf8nLCh.jpeg';
+function AddPdf() {
+  const { assetImageUrl } = useContext(ContractContext);
+  const screenshotRef = useRef(null);
+
+  const generatePDF = async () => {
+    const imgBlob = await fetch(assetImageUrl).then((res) => res.blob());
+    const doc = (
+      <Document>
+        <Page>
+          <Image src={URL.createObjectURL(imgBlob)} />
+        </Page>
+      </Document>
+    );
+    const pdfBlob = await pdf(doc).toBlob();
+    const url = URL.createObjectURL(pdfBlob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'my-image.pdf';
+    link.click();
+    URL.revokeObjectURL(url);
+  };
 
   return (
-    <div>
-      <PDFDownloadLink document={<MyDocument imageUrl={imageUrl} />} fileName="my-document.pdf">
-        {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-      </PDFDownloadLink>
+    <div ref={screenshotRef}>
+      <img src={assetImageUrl} alt="My Image" />
+      <Button variant="primary" onClick={generatePDF}>
+        Save as PDF
+      </Button>
     </div>
   );
-};
+}
 
-export default App;
+export default AddPdf;
