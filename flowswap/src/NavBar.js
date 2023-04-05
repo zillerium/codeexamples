57eb5aa260dffd789923e1bbec649051b4efdd7c
sales@merrows.co.uk <sales@merrows.co.uk>
2023-04-05 07:20:41 +0000

    add investments

--- flowswap/src/NavBar.js
+++ flowswap/src/NavBar.js
@@ -3,32 +3,32 @@ import flowswap from './flowswap.png';
 import {CartContext} from './CartContext';
 import {useContext} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Container, Button } from 'react-bootstrap';
 import {ArrowRight, WalletFill, Wallet2} from 'react-bootstrap-icons';
 
 const NavBar=() => {
 	const cart = useContext(CartContext);
 
-  return return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand>
-        <Link to="/" className="site-title">
-          <img src={flowswap} className="img-fluid" />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <CustomLink to="/admin">Register</CustomLink>
-          <CustomLink to="/portfolio">Portfolio</CustomLink>
-          <CustomLink to="/invest">Invest</CustomLink>
-          <CustomLink to="/investments">Investment</CustomLink>
-          <CustomLink to="/addasset">Add Asset</CustomLink>
-          <CustomLink to="/about">About</CustomLink>
-        </Nav>
-        <Nav>
-          <Button onClick={handleShow}>({productsCount} shares)</Button>
-        </Nav>
-      </Navbar.Collapse>
+  return (
+    <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" className="site-title">
+            <img src={flowswap} className="img-fluid" />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link to="/admin">Register</Nav.Link>
+            <Nav.Link to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link to="/invest">Invest</Nav.Link>
+            <Nav.Link to="/investments">Investment</Nav.Link>
+            <Nav.Link to="/addasset">Add Asset</Nav.Link>
+            <Nav.Link to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+	</Container>  
     </Navbar>
   );
 };
@@ -36,9 +36,9 @@ const NavBar=() => {
  
 
 
-}
 
-const CustomLink = ({to, children, ...props}) => {
+
+const CustomLinks = ({to, children, ...props}) => {
    const path = window.location.pathname;
 	const resolvedPath = useResolvedPath (to);
 	const isActive = useMatch({ path: resolvedPath.pathname, end:true});
