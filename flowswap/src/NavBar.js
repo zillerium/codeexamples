361a0a6435d169254c87947b6ff45997446b6c12
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-05 06:26:53 +0100

    Update NavBar.js

--- flowswap/src/NavBar.js
+++ flowswap/src/NavBar.js
@@ -1,42 +1,45 @@
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
-import flowswap from './flowswap.png';
-import {CartContext} from './CartContext';
-import {useContext} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {ArrowRight, WalletFill, Wallet2} from 'react-bootstrap-icons';
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import flowswap from "./flowswap.png";
+import { CartContext } from "./CartContext";
+import { useContext } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
-const NavBar=() => {
-	const cart = useContext(CartContext);
+const NavBar = ({ productsCount }) => {
+  const cart = useContext(CartContext);
 
-  return <nav className="nav">
-      <Link to="/" className="site-title"><img src={flowswap} className="img-fluid" /></Link>
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        <img src={flowswap} className="img-fluid" />
+      </Link>
       <ul>
-		<CustomLink to="/admin" >Register</CustomLink>
-		<CustomLink to="/portfolio" >Portfolio</CustomLink>
-		<CustomLink to="/invest" >Invest</CustomLink>
-		<CustomLink to="/investments" >Investment</CustomLink>
-		<CustomLink to="/addasset" >Add Asset</CustomLink>
-		<CustomLink to="/about" >About</CustomLink>
+        <CustomLink to="/admin">Register</CustomLink>
+        <CustomLink to="/portfolio">Portfolio</CustomLink>
+        <CustomLink to="/invest">Invest</CustomLink>
+        <CustomLink to="/investments">Investment</CustomLink>
+        <CustomLink to="/addasset">Add Asset</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
       </ul>
-  </nav>
+      <Button onClick={handleShow}>Investment ({productsCount} shares)</Button>
+    </nav>
+  );
+};
 
-
-}
-
-const CustomLink = ({to, children, ...props}) => {
-   const path = window.location.pathname;
-	const resolvedPath = useResolvedPath (to);
-	const isActive = useMatch({ path: resolvedPath.pathname, end:true});
-console.log("children = " + children);
-console.log("children = " + children);
-console.log("props = " + JSON.stringify({...props}));
-   return (
-      <li className={isActive ? "active" : ""}>
-         <Link to={to} {...props}>
-             {children}
-	 </Link>
-      </li>
-   )
-}
+const CustomLink = ({ to, children, ...props }) => {
+  const path = window.location.pathname;
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  console.log("children = " + children);
+  console.log("children = " + children);
+  console.log("props = " + JSON.stringify({ ...props }));
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+};
 
 export default NavBar;
