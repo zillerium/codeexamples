6e83b7670e0af23008a11855551a6d41c0b90e16
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-21 03:29:36 +0100

    Update arrows.js

--- doramo/src/arrows.js
+++ doramo/src/arrows.js
@@ -1,9 +1,10 @@
-onst PrevArrow = (props) => {
+const PrevArrow = (props) => {
   const { className, onClick } = props;
   return (
     <div
       className={className}
       onClick={onClick}
+      style={{ position: 'absolute', top: '50%', left: 0, zIndex: 1, cursor: 'pointer' }}
     >
       <i className="fas fa-chevron-left"></i>
     </div>
@@ -16,9 +17,9 @@ const NextArrow = (props) => {
     <div
       className={className}
       onClick={onClick}
+      style={{ position: 'absolute', top: '50%', right: 0, zIndex: 1, cursor: 'pointer' }}
     >
       <i className="fas fa-chevron-right"></i>
     </div>
   );
 };
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-0s9sZ7tj0D5fCw0QmmIvOyKxOyJGg0+8XpHbXjLH1GJLjJ8fNn0zv2x4y3Kz+8sWJ9s9sKjZmJgZ1tQ/sYtYJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
