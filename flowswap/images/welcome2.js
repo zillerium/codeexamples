763d9b0e8adf98422dd0a38bec44d60d37b3c41d
zillerium <23511764+zillerium@users.noreply.github.com>
2023-04-23 04:18:10 +0100

    Update welcome2.js

--- flowswap/images/welcome2.js
+++ flowswap/images/welcome2.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 const Welcome = () => {
   return (
     <div className="container">
-	    <Row className="mb-4">
+      <Row className="mb-4">
         <Col>
           <h1 className="text-center">Lixinato</h1>
           <p className="text-center lead">Blockchain solutions in Defi and decentralized ecommerce. Use the links below to find out more.</p>
@@ -47,22 +47,16 @@ const Welcome = () => {
             />
           </a>
         </Col>
-      </Row>
-      <Row className="justify-content-center">
         <Col xs={12} sm={6} md={6} lg={6} className="d-flex justify-content-center mb-4">
           <a href="https://chainera.network" target="_blank" rel="noopener noreferrer">
-            <Row>
-              <Col xs={12} md={3} lg={3}>
-                <img
-                  src="chainerascreen.png"
-                  alt="Blockchain Consultancy"
-                  className="img-fluid w-100"
-                />
-              </Col>
-              <Col xs={12} md={9} lg={9} className="d-flex align-items-center">
-                <span className="ml-2">Blockchain Consultancy</span>
-              </Col>
-            </Row>
+            <div className="d-flex flex-row align-items-center">
+              <img
+                src="chainerascreen.png"
+                alt="Blockchain Consultancy"
+                className="img-fluid w-100"
+              />
+              <span className="ml-2">Blockchain Consultancy</span>
+            </div>
           </a>
         </Col>
       </Row>
